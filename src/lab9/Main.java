d424abdeeb785cd94537b8b35ec91442805b9de7
Yingqi Zhong <yzhongbd@connect.ust.hk>
2022-04-29 21:57:45 +0800

    lab9 finished

---
+++ src/lab9/Main.java
@@ -0,0 +1,11 @@
+package lab9;
+import java.util.Scanner;
+
+import javafx.application.Application;
+
+public class Main {
+	public static void main(String args[])
+	{
+		Application.launch(UnresponsiveUI.class);
+	}
+}
